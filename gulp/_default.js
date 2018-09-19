6216992e427f2dcf22cca68d4e8e9a4506b5d2ea
Josh-Easter <josh.easter@ipreo.com>
2018-09-19 15:57:26 -0400

    map and value fixes

--- gulp/_default.js
+++ gulp/_default.js
@@ -11,22 +11,22 @@ theo.registerFormat("deep", result => {
     .first()
     .toJS();
   return `
-  $${category}-${type}: (
+  $${type}: (
   ${result
     .get("props")
     .map(
       prop => `
       '${prop.get("name")}': (
-        0: $${prop.get("name")}-0,
-        1: $${prop.get("name")}-1,
-        2: $${prop.get("name")}-2,
-        3: $${prop.get("name")}-3,
-        4: $${prop.get("name")}-4,
-        5: $${prop.get("name")}-5,
-        6: $${prop.get("name")}-6,
-        7: $${prop.get("name")}-7,
-        8: $${prop.get("name")}-8,
-        9: $${prop.get("name")}-9
+        0: $${prop.get("type")}-${prop.get("name")}-0,
+        1: $${prop.get("type")}-${prop.get("name")}-1,
+        2: $${prop.get("type")}-${prop.get("name")}-2,
+        3: $${prop.get("type")}-${prop.get("name")}-3,
+        4: $${prop.get("type")}-${prop.get("name")}-4,
+        5: $${prop.get("type")}-${prop.get("name")}-5,
+        6: $${prop.get("type")}-${prop.get("name")}-6,
+        7: $${prop.get("type")}-${prop.get("name")}-7,
+        8: $${prop.get("type")}-${prop.get("name")}-8,
+        9: $${prop.get("type")}-${prop.get("name")}-9
       ),`
     )
     .sort()
--- gulp/index.js
+++ gulp/index.js
@@ -18,51 +18,6 @@ var metaHeader = `
 /* ----------------------- */
 `;
 
-//Custom formatter for nested map
-theo.registerFormat("deep", result => {
-  let { category, type } = result
-    .get("props")
-    .first()
-    .toJS();
-  return `
-  $${category}-${type}: (
-  ${result
-    .get("props")
-    .map(
-      prop => `
-      '${prop.get("name")}': (
-        0: $${prop.get("name")}-0,
-        1: $${prop.get("name")}-1,
-        2: $${prop.get("name")}-2,
-        3: $${prop.get("name")}-3,
-        4: $${prop.get("name")}-4,
-        5: $${prop.get("name")}-5,
-        6: $${prop.get("name")}-6,
-        7: $${prop.get("name")}-7,
-        8: $${prop.get("name")}-8,
-        9: $${prop.get("name")}-9
-      ),`
-    )
-    .sort()
-    .join("\n")}
-  );
-  `;
-});
-
-
-theo.registerFormat('map.variables.scss', `
-$\{{stem meta.file}}: (
-  {{#each props as |prop|}}
-    {{kebabcase prop.name}}: $\{{kebabcase prop.type}}-{{kebabcase prop.name}},
-  {{/each}}
-  );`);
-
-  theo.registerFormat('default', `
-  {{#each props as |prop|}}
-  $\{{kebabcase prop.type}}-{{kebabcase prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}} !default;
-{{/each}}`);
-
-
 
 // This runs the task over the files making a map based on default variables
 gulp.task('tokens:map', (done) => {
@@ -135,7 +90,8 @@ gulp.task('clean:tokens', (done) => {
 
 gulp.task('tokens:merge', (done) => {
   gulp.src(['./dist/_variables.scss', './dist/_colors-map.scss', './dist/_maps.scss'])
-      .pipe(concat('index.scss'))
+      .pipe(vinylPaths(del))
+      .pipe(concat('_tokens.scss'))
       .pipe(addHeader(metaHeader))
       .pipe(gulp.dest(config.tokens.output))
 done();
